c02f51bfe69cbe3ac5a4c9391aa960216393b932
엄기산 <90389456+eomgisan@users.noreply.github.com>
2022-06-13 17:46:23 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// cout 의 소수점 출력 범위 : cout.precision(출력자릿수)
+// 이때 precision은 유효숫자의 갯수에 따라 소수점 범위가 달라질수도 있으니 cout << fixed 를 같이 써서 소수점 범위 고정
+// 정수 / 정수 = 정수로만 출력되므로 a,b 중 하나는 double로 형변환 해서 출력물을 double로 만들어야함
+// 형변환 방법 1 : (double)a;
+// 형변환 방법 2 : double(a);
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+    cout.precision(10);
+    cout << fixed;
+	cout << (double)a/(double)b << endl;
+	return 0;
+}
