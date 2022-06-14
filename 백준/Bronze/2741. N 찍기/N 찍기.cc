5e5d7caa14bc2954e613c278ac24176bf8e44f41
엄기산 <90389456+eomgisan@users.noreply.github.com>
2022-06-14 16:30:55 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t,i;
+    cin >> t;
+    
+    for(i=1;i<t+1;i++){
+       
+        cout << i << "\n";
+    }
+    
+    return 0;
+}
