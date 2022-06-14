b48832eeb87e62b5d44e3bfcfcab09a148127bec
엄기산 <90389456+eomgisan@users.noreply.github.com>
2022-06-14 16:32:17 +0900

    [Bronze III] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    for(t;t>0;t--){
+        
+        cout << t << "\n";
+    }
+    
+    return 0;
+}
