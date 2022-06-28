3b63e5285df5aa8028b0d50dbea4c3125ef25879
엄기산 <90389456+eomgisan@users.noreply.github.com>
2022-06-28 18:08:32 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		int result = 1;
+		for (int i = 1; i <= n; i++) {
+			result *= i;
+		}
+		return result;
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << factorial(n);
+	return 0;
+}
