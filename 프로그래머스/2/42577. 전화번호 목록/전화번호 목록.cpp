ba9543e56d24ffbf6d4f7263ef716613ac2a7065
엄기산 <90389456+eomgisan@users.noreply.github.com>
2023-11-01 17:53:22 +0900

    [level 2] Title: 전화번호 목록, Time: 73.69 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,31 +1,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <unordered_set>
+#include <iostream>
 using namespace std;
 
-bool solution(vector<string> phone_book) {
-    unordered_set<string> hashSet(phone_book.begin(), phone_book.end());
-/*
-    for (auto it1 = tmp.begin(); it1 != tmp.end(); ++it1){
-        size_t len_it1 = it1->size();
-        for(auto it2 = tmp.begin() ; it2 != tmp.end(); ++it2){
-            if(it1 == it2) continue;
-            
-            size_t subStringLen = min(len_it1, it2->size());
-            
-            if(it1->substr(0,subStringLen) == it2->substr(0,subStringLen)) return false;
-            
+bool solution(vector<string> phoneBook) {
+    bool answer = true;
+
+    sort(phoneBook.begin(), phoneBook.end());
+
+    for ( int i = 0 ; i < phoneBook.size() - 1 ; i++ )
+    {
+        if ( phoneBook[i] == phoneBook[i+1].substr(0, phoneBook[i].size()) )
+        {
+            answer = false;
+            break;
         }
     }
-    */
-    for(auto phone : phone_book){
-        string tmp = "";
-        for(auto ch : phone){
-            tmp += ch;
-            if(hashSet.find(tmp) != hashSet.end() && tmp != phone) return false;
-        }
-    }
-            
-    return true;
+
+    return answer;
 }
