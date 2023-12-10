aef000e257ca34f18de85a27d5b18a96df252639
엄기산 <90389456+eomgisan@users.noreply.github.com>
2023-12-11 03:23:36 +0900

    [Silver I] Title: 상어 키우기, Time: 60 ms, Memory: 3964 KB -BaekjoonHub

--- 백준/Silver/30892. 상어 키우기/상어 키우기.cc
+++ 백준/Silver/30892. 상어 키우기/상어 키우기.cc
@@ -1,59 +1,75 @@
 #include <iostream>
-#include <list>
+#include <queue>
+#include <vector>
+#include <stack>
 
 using namespace std;
 
-int main()
-{
+priority_queue<int> pq;
+stack<int> a;
+long long N, K, T, temp;
+void Eat();
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
-    int N, K;
-    long long T;
+    cout.tie(NULL);
     cin >> N >> K >> T;
-    list<int> arr(N);
-    list<int>::iterator iter;
-    for(list<int>::iterator iter = arr.begin() ; iter != arr.end() ; ++iter)
-    {
-        int temp; cin >> temp;
-        *iter = temp;
+    for (int i = 0; i < N; ++i) {
+        cin >> temp;
+        pq.push(temp);
     }
-    arr.sort(less<>()); 
 
-    
-    iter = arr.begin();
-    
-    while(K > 0 && iter != arr.end())
-    {
-        // 현재 iter의 값이 작을경우 증가
-        if(*(iter) < T)
-        {
-            ++iter;
+    while (!pq.empty()) {
+        temp = pq.top();
+        if (temp < T) {
+            break;
         }
-        
-        
-        else
-        {
-            // 현재 iter이 값이 클경우 이전꺼 먹기
-
-            // case 1 : 현재 iter이 begin일 경우 -> 먹을수 있는게 없으니 return
-            if(iter == arr.begin()) K = 0;
-            else{
-                // case 2 : begin이 아닐경우 -> 이전꺼 먹기
-              
-                T += *(--iter);
-                iter = arr.erase(iter);
-                --K;
-            }
-            
-        }
+        a.push(temp);
+        pq.pop();
     }
 
-    while(K-- > 0)
-    {
-        T += *(--iter);
-    }
+    Eat();
 
     cout << T;
     return 0;
+}
+
+void Eat() {
+    int t1, t2;
+    while (K) {
+        t2 = T; // t2 : 현재 크기
+        
+        // 큰것들중 먹을수 있는게 있는지 확인하기
+        while (!a.empty()) { // 현재 크기보다 큰것들이 빌때까지 반복
+            t1 = a.top(); // t1 : 큰것들중
+            if (t1 >= T)
+                // 먹을수 있는게 없으면 패스 
+                break;
+
+            else {
+                // 먹을수 있는게 있다?
+
+                // 만약 자기보다 작은게 있어? 그럼 옮겨줘 
+                if (!a.empty() && a.top() < T) {   // 기존엔 if( !a.empty() && a.top() < T )
+                    a.pop();
+                    pq.push(t1);
+                    // a.pop();
+                }
+
+                else {
+                    T += t1;
+                    --K;
+                }
+            }
+        }
+
+        if (K-- && !pq.empty()) {
+            T += pq.top();
+            pq.pop();
+        }
+
+        if (t2 == T)
+            break;
+    }
 }
